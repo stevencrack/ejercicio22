522f890c347c62ce951d7f205fa7025bb2921cd5
stevencrack <32911023+stevencrack@users.noreply.github.com>
2018-04-24 08:07:19 -0500

    Add files via upload

---
+++ gauus.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('datosgauus.txt')
+
+minimo = data[:,0].min()
+maximo = data[:,0].max()
+
+i_x = data[data[:,0]==minimo, 1]
+i_y = data[data[:,0]==minimo, 2]
+
+f_x = data[data[:,0]==maximo, 1]
+f_y = data[data[:,0]==maximo, 2]
+
+plt.plot(i_x, i_y, label='inicial')
+plt.plot(f_x, f_y, '--', label='final')
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('Grafica de Gauss')
+plt.savefig('gauss.png')
+plt.show()
